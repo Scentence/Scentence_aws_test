d3b6877e5f3732fed8502904ac199afd1d56f4be
sosodoit <rosesh0218@gmail.com>
2026-01-22 12:26:28 +0900

    feat: 향수맵

--- scentmap/db.py
+++ scentmap/db.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("PERFUME_DATABASE_URL", "")
-DB_CONFIG = {
-    "dbname": os.getenv("PERFUME_DB_NAME", "perfume_db"),
-    "user": os.getenv("PERFUME_DB_USER", "scentence"),
-    "password": os.getenv("PERFUME_DB_PASSWORD", "scentence"),
-    "host": os.getenv("PERFUME_DB_HOST", os.getenv("DB_HOST", "localhost")),
-    "port": os.getenv("PERFUME_DB_PORT", os.getenv("DB_PORT", "5432")),
-}
 
+DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME", "perfume_db"),
+    "user": os.getenv("DB_USER", "scentence"),
+    "password": os.getenv("DB_PASSWORD", "scentence"),
+    "host": os.getenv("DB_HOST", "host.docker.internal"),
+    "port": os.getenv("DB_PORT", "5435"),
+}
 
 def get_db_connection():
     if DATABASE_URL:
